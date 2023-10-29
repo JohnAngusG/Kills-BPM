5f2855d7e89be0f1b17ffffd7fd03b3265448aed
John Mullaney <j.a.mullaney1@gmail.com>
2023-10-29 12:04:00 -0700

    Fix return type of functions.
    Functions were returning type non as I had them print the return value
    for bug testing. Forgot to switch it back to returning value. Corrected.

--- get_data.py
+++ get_data.py
@@ -6,12 +6,17 @@ def get_puuid(region, api_key, summoner_name):
     repsonse = requests.get(api_url)
     player_info = repsonse.json()
     player_puuid = player_info["puuid"]
-    return print(player_puuid)
+    return str(player_puuid)
+
 
 def get_matches(puuid, api_key):
-    api_url = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid + "&api_key=" + api_key
+    api_url = "https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/" + puuid + "/ids?start=0&count=20" +"&api_key=" + api_key
     repsonse = requests.get(api_url)
     match_ids = repsonse.json()
-    return print(match_ids)
+    return str(match_ids)
 
 
+puuid = get_puuid("na1", config.api_key, "M4NU")
+
+matches = get_matches(puuid, config.api_key)
+print(matches)
